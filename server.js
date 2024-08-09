c146e01b9b91d6bd9c00aa02ce861408c42a8da9
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2024-08-08 22:40:05 -0300

    back3

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const { chromium } = require('playwright'); // Importa Playwright
-const fs = require('fs'); // Importa el módulo del sistema de archivos
+const { chromium } = require('playwright');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para servir archivos estáticos
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
 const scrapeData = async () => {
-  // Inicia el navegador y abre una nueva página
   const browser = await chromium.launch();
   const page = await browser.newPage();
   
-  // Navega a la página deseada
   await page.goto('https://bolsar.info/Cedears.php');
-  // Espera a que el elemento 'Variación' esté visible
   await page.waitForSelector('text=Variación');
-  // Hace clic en el elemento que contiene la palabra 'Variación' para obtener los negativos
   await page.click('text=Variación');
 
   const buildArray = () => {
@@ -27,26 +22,20 @@ const scrapeData = async () => {
       return {
         nombre: tds[0] ? tds[0].innerText.trim() : null,
         variacion: tds[7] ? tds[7].innerText.trim() : null
-        // Puedes agregar más campos aquí según sea necesario
       };
-    }).filter(item => item.nombre); // Filtra las filas que no tienen 'nombre'
+    }).filter(item => item.nombre);
   };
 
-  // Realiza el scraping y guarda los datos negativos CEDEAR
   const dataNegative = await page.evaluate(buildArray);
   await page.click('text=Variación');
-  // Realiza el scraping y guarda los datos positivos CEDEAR
   const dataPositive = await page.evaluate(buildArray);
 
-  // Escribe los datos en un archivo JSON
   fs.writeFileSync(path.join(__dirname, 'src', 'dataNegative.json'), JSON.stringify(dataNegative, null, 2));
   fs.writeFileSync(path.join(__dirname, 'src', 'dataPositive.json'), JSON.stringify(dataPositive, null, 2));
 
-  // Cierra el navegador
   await browser.close();
 };
 
-// Función para cargar los datos desde los archivos JSON si existen
 const loadDataFromFiles = () => {
   const dataNegativePath = path.join(__dirname, 'src', 'dataNegative.json');
   const dataPositivePath = path.join(__dirname, 'src', 'dataPositive.json');
@@ -60,14 +49,11 @@ const loadDataFromFiles = () => {
   }
 };
 
-// Variables para almacenar los datos en memoria
 let dataNegative = null;
 let dataPositive = null;
 
-// Carga los datos desde los archivos al iniciar el servidor
 loadDataFromFiles();
 
-// Realiza el scraping al iniciar el servidor si los archivos JSON no existen o están vacíos
 if (!dataNegative || !dataPositive) {
   scrapeData().then(() => {
     console.log('Datos iniciales cargados mediante scraping.');
@@ -76,12 +62,26 @@ if (!dataNegative || !dataPositive) {
   });
 }
 
-// Ruta para la raíz
+const checkMarketOpenAndScrape = async () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  const currentDay = now.getDay();
+
+  if (currentDay >= 1 && currentDay <= 5 && currentHour >= 11 && currentHour < 17) {
+    console.log('El mercado está abierto. Realizando scraping...');
+    await scrapeData();
+    console.log('Datos actualizados.');
+  } else {
+    console.log('El mercado está cerrado.');
+  }
+};
+
+setInterval(checkMarketOpenAndScrape, 60 * 60 * 1000);
+
 app.get('/', (req, res) => {
   res.send('Bienvenido a mi API de scraping');
 });
 
-// Ruta para iniciar el scraping manualmente
 app.get('/scrape', async (req, res) => {
   try {
     await scrapeData();
@@ -91,7 +91,6 @@ app.get('/scrape', async (req, res) => {
   }
 });
 
-// Ruta para obtener los datos negativos
 app.get('/dataNegative', (req, res) => {
   if (dataNegative) {
     res.json(dataNegative);
@@ -100,7 +99,6 @@ app.get('/dataNegative', (req, res) => {
   }
 });
 
-// Ruta para obtener los datos positivos
 app.get('/dataPositive', (req, res) => {
   if (dataPositive) {
     res.json(dataPositive);
